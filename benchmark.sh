82dc8e311deba45a1e5602b12db6bc89558ecd53
ISHIDA Akio <iakio.monospacenet@gmail.com>
2016-12-31 22:44:38 +0900

    fix

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 composer require laravel/framework && \
-phpunit --bootstrap=vendor/autoload.php --repeat=10 --stop-on-error tests && \
+phpunit -q --bootstrap=vendor/autoload.php --repeat=100 --stop-on-error tests && \
 composer require laravel/framework dev-pgsql_improve && \
-phpunit --bootstrap=vendor/autoload.php --repeat=10 --stop-on-error tests
+phpunit -q --bootstrap=vendor/autoload.php --repeat=100 --stop-on-error tests
